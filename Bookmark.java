034ce0df00f096cb9a735c5d00d6e0c44990c103
Dongwook <dwchoi0610@gmail.com>
2022-04-13 18:44:46 +0900

    deleted BookmarkProgram, applied private

--- Bookmark.java
+++ Bookmark.java
@@ -1,26 +1,13 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
 
 public class Bookmark {
-	String name;
-	String addedTime;
-	String url;
-	String groupName;
-	String memo;
-	Scanner inputScanner;
+	private String name; // 북마크 이름
+	private String addedTime; // 북마크가 추가된 시간
+	private String url; // 북마크 url
+	private String groupName; // 북마크 그룹명
+	private String memo; // 북마크 메모
 	
-	Bookmark(String name, String url, String groupName, String memo){
-		this.name = name;
-		this.url = url;
-		this.groupName = groupName;
-		this.memo = memo;
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter newPattern = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm");
-		addedTime = now.format(newPattern);
-	}
-	
-	Bookmark(String name, String addedTime, String url, String groupName, String memo){ // file에서 읽어 생성 시 사용
+	Bookmark(String name, String addedTime, String url, String groupName, String memo){ 
+		// file에서 값을 읽어서 객체를 만들 때 사용
 		this.name = name;
 		this.addedTime = addedTime;
 		this.url = url;
@@ -28,8 +15,14 @@ public class Bookmark {
 		this.memo = memo;
 	}
 	
-	public void print() {
+	public void print() { // bookmark 출력 메서드
 		System.out.print(name+","+addedTime+","+url+","+groupName+","+memo+"\n");
 	};	
 	
+	public String getName() {return name;}
+	public String getAddedTime() {return addedTime;}
+	public String getUrl() {return url;}
+	public String getGroupName() {return groupName;}
+	public String getMemo() {return memo;}
+	// 멤버변수의 getter 함수
 }
--- Test.java
+++ Test.java
@@ -1,19 +1,14 @@
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException {
 		BookmarkList newBookmarkList;
 		newBookmarkList = new BookmarkList("bookmark-normal.txt");
-		for (int i = 0; i < newBookmarkList.bookmarkCounter; ++i) newBookmarkList.bookmarkList[i].print();
+		//for (int i = 0; i < newBookmarkList.bookmarkCounter; ++i) newBookmarkList.bookmarkList[i].print();
 		
 		System.out.println("\n\n\n");
 		newBookmarkList.mergeByGroup();
-	//	for (int i = 0; i < newBookmarkList.bookmarkCounter; ++i) newBookmarkList.bookmarkList[i].print();
 	}
-
 }
 
